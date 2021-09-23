478509de935070b9148b743ccfcea8660d83fc4f
ABUJUBAIR111 <jubair103411@gmail.com>
2021-09-23 15:27:58 +0600

    Laravel Complete Ecommerce Website

---
+++ resources/views/frontend/pages/contact.blade.php
@@ -0,0 +1,11 @@
+@extends('frontend.layouts.master')
+
+@section('content')
+  <div class='container margin-top-20'>
+    <h1>Contact Page</h1>
+    <h2>Email</h2>
+    <p>jubair103411@gmail.com</p>
+    <h2>Phone</h2>
+     <p>01765947454</p>
+  </div>
+@endsection
